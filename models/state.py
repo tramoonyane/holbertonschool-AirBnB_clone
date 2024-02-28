c94d65f3828a52ccce9efb0a646616375fa0f00a
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-02-28 18:18:26 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+"""Module for State class"""
+from models.base_model import BaseModel
 
+
+class State(BaseModel):
+    """Defines attributes for the State class"""
+    name = ""
