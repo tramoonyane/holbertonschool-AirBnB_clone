4e5b653af138a5f33253a8c353355f281ac241e9
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-02-28 18:18:53 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""Module for User class"""
 
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Defines attributes for the User class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
