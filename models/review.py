95138a9b5e4bde47043c337ecd8c406c2ddca5f1
Makhothi <147424358+Makhothi@users.noreply.github.com>
2024-02-28 18:17:56 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+"""Module for Review class"""
+from models.base_model import BaseModel
 
+
+class Review(BaseModel):
+    """Defines attributes for the Review class"""
+    place_id = ""
+    user_id = ""
+    text = ""
