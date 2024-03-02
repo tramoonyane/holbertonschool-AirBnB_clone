4dc5c9f6ce2ff4dff5130ef547858f4280aed81d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-03 00:58:10 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -67,6 +67,17 @@ class TestHBNBCommand_help(unittest.TestCase):
             expected_output = h.replace('\n', '\n        ')
             self.assertEqual(expected_output, output.getvalue().strip())
 
+    def test_help_show(self):
+        h = (
+            "Show the string representation of an instance.\n"
+            "Usage: show <class_name> <id>"
+        )
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help show"))
+            # Adjust the expected string to match the indentation of the actual output
+            expected_output = h.replace('\n', '\n        ')
+            self.assertEqual(expected_output, output.getvalue().strip())
+
 
 class TestHBNBCommand_exit(unittest.TestCase):
     """Unittests for testing exiting from the HBNB command interpreter."""
