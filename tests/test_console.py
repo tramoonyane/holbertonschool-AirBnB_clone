5c4b069cc65adb14b1bb7a43ce76e15e93a132fc
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-03 01:02:15 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -89,6 +89,17 @@ class TestHBNBCommand_help(unittest.TestCase):
             expected_output = h.replace('\n', '\n        ')
             self.assertEqual(expected_output, output.getvalue().strip())
 
+    def test_help_all(self):
+        h = (
+            "Print the string representation of all instances or a specific class.\n"
+            "Usage: <User>.all()"
+        )
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help all"))
+            # Adjust the expected string to match the indentation of the actual output
+            expected_output = h.replace('\n', '\n        ')
+            self.assertEqual(expected_output, output.getvalue().strip())
+
 
 class TestHBNBCommand_exit(unittest.TestCase):
     """Unittests for testing exiting from the HBNB command interpreter."""
