8314a83342abde8db73044c7fa8c8e6c02949b86
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-03-03 03:01:07 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -103,6 +103,12 @@ class TestHBNBCommand_help(unittest.TestCase):
             # Adjust the expected string to match
             # the indentation of the actual output
             expected_output = h.replace('\n', '\n        ')
+            # Update the expected message to reflect the new help message
+            expected_output = expected_output.replace(
+                'Update an instance by adding or updating an attribute.',
+                'Update an instance by adding or updating an attribute.\n'
+                'Usage: update <class_name> <id> <attribute_name> "<attribute_value>"'
+            )
             self.assertEqual(expected_output, output.getvalue().strip())
 
 
